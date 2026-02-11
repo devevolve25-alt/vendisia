ede2b849ca77225f76925cd53c82c043ae4de20d
devevolve25-alt <dev.evolve.25@gmail.com>
2026-02-11 18:52:08 -0300

    Update agenda.js

--- agenda.js
+++ agenda.js
@@ -1,4 +1,4 @@
-// 1. Configuração (Sempre no topo) - atualizado agenda dono - 3
+// 1. Configuração (Sempre no topo) - atualizado agenda dono - 4
 const SUPABASE_URL = 'https://zplqlcvcpeohtxodvfkq.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_YwQnRSNbTfXKnzTAbVWXGw_x8Zs2oK4';
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);

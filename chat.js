ea547dc478b173a9d85d49dc50ee123411f9f98a
devevolve25-alt <dev.evolve.25@gmail.com>
2026-02-12 23:42:34 -0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,4 +1,3 @@
-// 1. Configuração Supabase
 const SUPABASE_URL = 'https://zplqlcvcpeohtxodvfkq.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_YwQnRSNbTfXKnzTAbVWXGw_x8Zs2oK4';
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);

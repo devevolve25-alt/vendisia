bfbd7469f5aeeae61c287c6c137c6ac2bd2faad8
devevolve25-alt <dev.evolve.25@gmail.com>
2026-02-12 23:17:33 -0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -2,6 +2,7 @@
 const SUPABASE_URL = 'https://zplqlcvcpeohtxodvfkq.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_YwQnRSNbTfXKnzTAbVWXGw_x8Zs2oK4';
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
 const N8N_WEBHOOK_URL = 'https://powerfulkiwi-n8n.cloudfy.live/webhook/mercuria.sls-agnt';
 
 const chatWindow = document.getElementById('chat-window');

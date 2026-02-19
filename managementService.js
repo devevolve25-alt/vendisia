2db6dbfd1ff28482877cf06fee42bddf23deb64a
devevolve25-alt <dev.evolve.25@gmail.com>
2026-02-19 17:38:22 -0300

    Update managementService.js

--- managementService.js
+++ managementService.js
@@ -1,4 +1,4 @@
-/ managementService.js
+// managementService.js
 // Lógica de negócios e acesso a dados para gestão de estabelecimento, serviços e profissionais.
 
 import { supabaseClient } from './config.js';

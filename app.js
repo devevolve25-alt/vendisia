997d99ba4e72bc36c35af5560c1f22d93f800e13
devevolve25-alt <dev.evolve.25@gmail.com>
2026-02-23 17:03:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// app.js - Ponto de entrada e orquestrador da aplicação - corr 7
+// app.js - Ponto de entrada e orquestrador da aplicação - corr 8
 
 // Importa todos os módulos necessários
 import * as UI from './ui.js';
